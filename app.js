f4eaa42c1920ff09f9781b063ca396700f73ded3
davidreband <david.reband@gmail.com>
2018-07-12 12:40:48 +0200

    instal proj

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();aaa.html
+var path = require('path');
+var CLIENT_PATH = '/bower_components';
+app.use('/', express.static( path.join(__dirname, CLIENT_PATH )));
+
+var server =app.listen( 3000, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log( 'listening %s:%s', host,port);
+})
---
+++ bower_components/posts.js
@@ -0,0 +1,11 @@
+$( document ).ready( function(){
+
+    console.log("aaa");
+   // var postTemplate =  Handlebars.compile( $( '#post-template').html());
+    var postPreviewTemplate = Handlebars.compile( $( '#post-preview-template' ).html() );
+    var postsTemplate = Handlebars.compile( $( '#posts-container__list-template' ).html() );
+   // var navigationTemplate = Handlebars.compile( $( '#posts-container__navigation-templat' ).html() );
+
+    
+
+});
